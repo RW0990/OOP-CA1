9cb6a387c50b3c5870928c311c7a64eb48f969f5
Tara <x24158216@student.ncirl.ie>
2025-11-14 10:55:30 +0000

    mood tracker method

--- MoodTracker.java
+++ MoodTracker.java
@@ -1,19 +1,27 @@
 // file: com/example/sleeptracker/MoodTracker.java
-
-
 public class MoodTracker {
-    private double moodRating; 
-    private String notes;
+
+    private double moodRating; //mood rating 1-10 (10 being best)
+    private String notes; //user entered notes on why they may feel that way
 
     public void setMood(double rating, String notes) {
         this.moodRating = rating;
-        this.notes = notes; 
+        this.notes = notes;
     }
 
-    public double getMood() { return moodRating; }
-
+    public double getMood() {
+        return moodRating;
+    }
+    
+    //method to compare sleep with mood
     public String moodSleepCorrelation(double avSleep) {
-        return " ";
+        if (avSleep < 5) { //if less than 5 hours
+            return "You need more sleep, this may effect your mood";
+        } else if (avSleep >=5 && avSleep <=7) { //if more than 5 but less than or equal to 7 hours
+            return "This is not a bad amount of sleep, but we recommend trying to get at least 8 hours";            
+        } else {
+            return "Good job, you reached the sleep goal!";
+        }
     }
 
     @Override
